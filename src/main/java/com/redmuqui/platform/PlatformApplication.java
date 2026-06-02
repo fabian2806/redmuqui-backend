ac7db4d36b0be8c438109aee1108e23da65dfca1
claraaqwq <claraguevara1283@gmail.com>
2026-06-02 16:19:34 -0500

    Cerrar sesión del usuario, eliminar el token

--- src/main/java/com/redmuqui/platform/PlatformApplication.java
+++ src/main/java/com/redmuqui/platform/PlatformApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * Punto de entrada de la aplicación.
  *
@@ -12,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class PlatformApplication {
 
     public static void main(String[] args) {
